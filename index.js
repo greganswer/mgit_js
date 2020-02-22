9b6992753ed2b8fafb0ba1732517be3d79028afc
Greg Answer <greganswer@gmail.com>
2020-02-22 01:17:41 -0500

    Initial handler setup

--- index.js
+++ index.js
@@ -1,3 +1,60 @@
 const chalk = require('chalk');
-const clear = require('clear');
 const figlet = require('figlet');
+const program = require('commander');
+
+const pkg = require('./package.json');
+const handle = require('./lib/handle');
+
+program
+  .name(pkg.name)
+  .version(pkg.version)
+  .option('-d, --debug', 'output extra debugging');
+
+program
+  .command('init')
+  .description('Initialize local repository and push to remote')
+  .action(handle.init);
+
+program
+  .command('branch <issueId>')
+  .description('Create a branch using issue ID and title')
+  .option('-b, --base-branch, <baseBranch>', 'The base branch to perform this action on')
+  .action(handle.branch);
+
+program
+  .command('commit')
+  .description('Create a commit and push to GitHub')
+  .option('-m, --message, <message>', 'The commit message')
+  .option('--id, --issue-id, <issueId>', 'The ID of the issue being worked on')
+  .action(handle.commit);
+
+program
+  .command('pr')
+  .description('Create a GitHub Pull Request for the specified branch')
+  .option('-b, --base-branch, <baseBranch>', 'The base branch to perform this action on')
+  .action(handle.pullRequest);
+
+program
+  .command('open')
+  .description('Open the issue in the default browser')
+  .action(handle.open);
+
+program
+  .command('*')
+  .action(() => program.help());
+
+program.parse(process.argv);
+
+if (program.debug) {
+  console.log(program.opts());
+}
+
+if (!process.argv.slice(2).length) {
+  console.log(
+    chalk.yellow(
+      figlet.textSync(pkg.name, { horizontalLayout: 'full' })
+    ),
+  );
+  console.log()
+  program.help();
+}
